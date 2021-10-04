aed22223645958dfad1c3de761cf639cb140e565
Sami EZZEROUALI <sami.ezzerouali@hotmail.com>
2021-10-04 23:07:15 +0200

    csv2json

--- .sh
+++ .sh
@@ -4,3 +4,12 @@
 function loop(){
  while true; do clear && $1 && sleep $2;done
 }
+
+function csv2json () {
+  python3 -c "
+    exec('''
+      import csv,json
+      print(json.dumps(list(csv.reader(open(\'${1}\')))))
+    ''')
+  "
+}
